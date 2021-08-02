25251aa24e95430198af2f3f3b6ab6fa1e3c5b5d
thatDingus <lolpatsalt@gmail.com>
2021-08-03 10:15:07 +1200

    new website swag cool
    sheeesh

--- README.md
+++
@@ -1,5 +0,0 @@
-# thatDingus.github.io
-
-A website im creating for myself because github is the webhosting equivalent of free real estate.
-
-https://thatDingus.github.io
---
+++ hover.js
@@ -0,0 +1,46 @@
+var currentpage = 1
+const inanim =  "animate__rotateInDownLeft"
+const outanim =  "animate__rotateOutDownLeft"
+const inanimback =  "animate__rotateInUpLeft"
+const outanimback =  "animate__rotateOutUpLeft"
+
+function pagedown() {
+    firstpage = document.getElementById("greeter-page-" + currentpage);
+    secondpage = document.getElementById("greeter-page-" + (currentpage + 1))
+    firstpage.classList.add(outanimback)
+    firstpage.addEventListener('animationend', () => {
+        firstpage.classList.remove("disp")
+        firstpage.classList.add("nodisp")
+        firstpage.classList.remove(outanimback)
+    });
+    secondpage.classList.add("disp")
+    secondpage.classList.remove("nodisp")
+    secondpage.classList.add(inanimback)
+    secondpage.addEventListener('animationend', () => {
+        secondpage.classList.remove(inanimback);
+    });
+    currentpage = currentpage + 1
+    numberpage.addEventListener('animationend', () => {
+        numberpage.innerHTML = currentpage
+        numberpage.classList.remove(outanimback)
+    });
+}
+
+function pageup() {
+    firstpage = document.getElementById("greeter-page-" + currentpage);
+    secondpage = document.getElementById("greeter-page-" + (currentpage - 1))
+    firstpage.classList.add(outanim)
+    firstpage.addEventListener('animationend', () => {
+        firstpage.classList.remove("disp")
+        firstpage.classList.add("nodisp")
+        firstpage.classList.remove(outanim)
+    });
+    secondpage.classList.add("disp")
+    secondpage.classList.remove("nodisp")
+    secondpage.classList.add(inanim)
+    secondpage.addEventListener('animationend', () => {
+        secondpage.classList.remove(inanim);
+    });
+    currentpage = currentpage - 1
+    document.getElementById("pagenumber").innerHTML = currentpage
+}
